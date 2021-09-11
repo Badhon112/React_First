07b78effab3847e1d7e5a17b171da02f21831fc0
Badhon <biswasbadhon896@gmail.com>
2021-09-11 20:21:52 +0600

    master

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import ReactDom from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function From(){
+    return(
+    <div className="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ex.</p>
+    </div>
+    );
+}
+export default From;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import From from './App.js';
+function Hi(){
+  return(
+    <div>
+      <From/>
+    </div>
+
+  );
+};
+ReactDom.render(<Hi/>,document.getElementById("root"));
