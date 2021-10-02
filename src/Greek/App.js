264a817d0f3faff7a1849b3c041a99a7d6b85fb8
Badhon <biswasbadhon896@gmail.com>
2021-10-02 18:38:35 +0600

    master\

---
+++ src/Greek/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="container my-3">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, animi.</p>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import App2 from "./App2";
-// import From from './App.js';
-// import Light from "./DarkMod";
-import From_dark from "./From_dark.js";
-function Hi() {
+import App from './Greek/App';
+function Index() {
   return (
     <div>
-      {/* <Light/> */}
-      {/* <From title="Text Form"/> */}
-      {/* <From_dark title="A From" /> */}
-      <App2/>
+      
+      <App/>
     </div>
   );
 }
-ReactDom.render(<Hi />, document.getElementById("root"));
+ReactDom.render(<Index />, document.getElementById("root"));
