a53c2b59c6469abc3f9371aa274c5d585676b4f0
Badhon <biswasbadhon896@gmail.com>
2021-10-20 23:26:27 +0600

    Add Twilwind Css

--- src/Greek/App.js
+++ src/Greek/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
     render() {
         return (
-            <div className="container my-3">
+            <div className="container bg-indigo-400 text-center ">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, animi.</p>
             </div>
         )
