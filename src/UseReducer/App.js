f034ecdd95a9ead098e6ab34d787362f43fdba95
Badhon <biswasbadhon896@gmail.com>
2021-11-22 15:41:18 +0600

    Use Reducer in React

--- src/UseReducer/App.js
+++ src/UseReducer/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import Counter from './Counter'
-
+// import Counter from './Counter'
+import Counter3 from './Counter3'
 export default class App extends Component {
     render() {
         return (
             <div className="m-16">
-                <Counter/>
+                {/* <Counter/> */}
+                <Counter3/>
             </div>
         )
     }
--- src/UseReducer/Counter3.js
+++ src/UseReducer/Counter3.js
@@ -1,27 +1,39 @@
 import React, { useReducer } from "react";
-
 const initialState = 0;
 const reducer = (state, action) => {
   switch (action) {
     case "Increment":
       return state + 1;
+
     case "Decrement":
       return state - 1;
+
     default:
       return state;
   }
 };
-export default function Counter2() {
+export default function Counter3() {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
       <div>Counter {state}</div>
-      <button type="button" onClick={()=>{
-          dispatch("Increment")
-      }} >Increment</button>
-      <button type="button" onClick={()=>{
-          dispatch("Decrement")
-      }} >Decrement</button>
+      <button
+      className="bg-gray-400"
+        type="button"
+        onClick={() => {
+          dispatch("Increment");
+        }}
+      >
+        Increment
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch("Decrement");
+        }}
+      >
+        Decrement
+      </button>
     </div>
   );
 }
